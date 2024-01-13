de0ba0f06a14043aa8b3e46b7b298dae22ab2f6a
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2024-01-13 15:05:39 +0530

    implement createTask method

--- to-do-app-back-end/src/main/java/lk/ijse/dep11/web/api/TaskHttpController.java
+++ to-do-app-back-end/src/main/java/lk/ijse/dep11/web/api/TaskHttpController.java
@@ -3,10 +3,12 @@ package lk.ijse.dep11.web.api;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep11.web.to.TaskTo;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.*;
 
 @RestController
 @RequestMapping("api/v1/tasks")
@@ -24,8 +26,26 @@ public class TaskHttpController {
         pool = new HikariDataSource(config);
     }
 
-    @PostMapping
-    public void createTask(){
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json",consumes = "application/json")
+    public TaskTo createTask(@RequestBody @Validated(TaskTo.Create.class) TaskTo task){
+
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection
+                    .prepareStatement("INSERT INTO task (description, status, email) VALUES (?, FALSE, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, task.getDescription());
+            stm.setString(2, task.getEmail());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            task.setId(id);
+            task.setStatus(false);
+            return task;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
