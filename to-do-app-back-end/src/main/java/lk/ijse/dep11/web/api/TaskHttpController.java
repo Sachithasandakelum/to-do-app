be445e16a80bec69c33af5342d711aea94a4d93c
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2024-01-13 14:41:54 +0530

    configure DBCP by constructor injection

--- to-do-app-back-end/src/main/java/lk/ijse/dep11/web/api/TaskHttpController.java
+++ to-do-app-back-end/src/main/java/lk/ijse/dep11/web/api/TaskHttpController.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep11.web.api;
 
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/tasks")
 @CrossOrigin
 public class TaskHttpController {
+
+    HikariDataSource pool;
+    public TaskHttpController() {
+        HikariConfig config = new HikariConfig();
+        config.setUsername("postgres");
+        config.setPassword("sachitha18");
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/dep11_todo_app");
+        config.setDriverClassName("org.postgresql.Driver");
+        config.addDataSourceProperty("maximumPoolSize",10);
+        pool = new HikariDataSource(config);
+    }
+
     @PostMapping
     public void createTask(){
 
